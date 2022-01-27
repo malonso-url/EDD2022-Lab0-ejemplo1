c6613214995a1a1122d9af4ce07de8069ff0939f
Moises Alonso <moises.alonso@telus.com>
2022-01-27 08:06:55 -0600

    Controller modfied

--- Lab0/Lab0/Controllers/HomeController.cs
+++ Lab0/Lab0/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Lab0.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+		
+		[HttpPost]
+		public String HelloWorld(String firstname, String lastname){
+			return "Hello " + firstname + " " + lastname;
+		}
     }
 }

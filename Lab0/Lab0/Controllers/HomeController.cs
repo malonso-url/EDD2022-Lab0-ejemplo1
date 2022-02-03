37a7c947e4aeb1b97969f22eaed4591a894d5ca9
Moises Alonso <maalonso@uvg.edu.gt>
2022-02-03 08:13:05 -0600

    Ejemplo en clase

--- Lab0/Lab0/Controllers/HomeController.cs
+++ Lab0/Lab0/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace Lab0.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public static LinkedList<Automovil> automovils = new LinkedList<Automovil>();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -35,8 +36,29 @@ namespace Lab0.Controllers
         }
 		
 		[HttpPost]
-		public String HelloWorld(String firstname, String lastname){
-			return "Hello " + firstname + " " + lastname;
+		public IActionResult HelloWorld(String firstname, String lastname){
+            ViewBag.firstname = firstname;
+            ViewBag.lastname = lastname;
+            ViewBag.id = (new Random(DateTime.Now.Millisecond)).Next(1000, 9999);
+            return View();
 		}
+
+        public IActionResult FormCar()
+        {
+            return View();
+        }
+
+        public IActionResult SaveCar(String Placa, String Marca, int Modelo) {
+            Automovil nuevoAuto = new Automovil(Placa, Marca, Modelo);
+            automovils.AddLast(nuevoAuto);
+            ViewBag.savecarsuccess = "Auto con placa: " + Placa + " Guardado exitosamente ";
+            return View();
+        }
+
+        public IActionResult ShowCar()
+        {
+            ViewData["autos"] = automovils;
+            return View();
+        }
     }
 }
---
+++ Lab0/Lab0/Models/Automovil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0.Models
+{
+    public class Automovil
+    {
+        public Automovil(String Placa, String Marca, int Modelo) {
+            this.Placa = Placa;
+            this.Marca = Marca;
+            this.Modelo = Modelo;
+        }
+
+        public string Placa {get; set;}
+
+        public string Marca { get; set; }
+
+        public int Modelo { get; set; }
+    }
+}
